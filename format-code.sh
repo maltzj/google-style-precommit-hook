9442193153e6a6ee7d32c00f1132c9da64206402
Yashashree Kokje <yashashree.k@gmail.com>
2018-01-25 02:23:01 -0800

    Fix variable assignment to include all grep matches

--- format-code.sh
+++ format-code.sh
@@ -8,6 +8,6 @@ then
 fi
 cd ..
 
-changed_java_files=($(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" ))
+changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
 echo $changed_java_files
 java -jar .cache/google-java-format-1.5-all-deps.jar --replace $changed_java_files
