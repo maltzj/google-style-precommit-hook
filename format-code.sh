bca60234147ac7ad7cc0b9a9b915ad5d3e63cc4a
Guilherme Peixoto <guilherme.peixoto@inlocomedia.com>
2018-09-10 13:37:40 -0300

    chore(format-code.sh): upgrade google-java-format version to 1.6

--- format-code.sh
+++ format-code.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 mkdir -p .cache
 cd .cache
-if [ ! -f google-java-format-1.5-all-deps.jar ]
+if [ ! -f google-java-format-1.6-all-deps.jar ]
 then
-    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-1.5/google-java-format-1.5-all-deps.jar"
-    chmod 755 google-java-format-1.5-all-deps.jar
+    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-1.6/google-java-format-1.6-all-deps.jar"
+    chmod 755 google-java-format-1.6-all-deps.jar
 fi
 cd ..
 
 changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
 echo $changed_java_files
-java -jar .cache/google-java-format-1.5-all-deps.jar --replace $changed_java_files
+java -jar .cache/google-java-format-1.6-all-deps.jar --replace $changed_java_files
