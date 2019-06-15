d5df04ebd3800f2e826c02c690cc12304792a2fd
Yi Wang <yi.wang.2005@gmail.com>
2019-06-15 11:33:48 -0700

    Update format-code.sh

--- format-code.sh
+++ format-code.sh
@@ -3,7 +3,7 @@ mkdir -p .cache
 cd .cache
 if [ ! -f google-java-format-1.7-all-deps.jar ]
 then
-    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-1.6/google-java-format-1.7-all-deps.jar"
+    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar"
     chmod 755 google-java-format-1.7-all-deps.jar
 fi
 cd ..
