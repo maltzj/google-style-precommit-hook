53cb1b190d3c0c79c4717023e61a6c45432ec8f8
Hiren Hiranandani <hhiranandani@spotnana.com>
2023-03-01 12:36:38 -0800

    fixup

--- format-code.sh
+++ format-code.sh
@@ -3,7 +3,7 @@ mkdir -p .cache
 cd .cache
 if [ ! -f google-java-format-1.16.0-all-deps.jar ]
 then
-    curl -LJO "https://github.com/google/google-java-format/releases/download/v1.15.0/google-java-format-1.15.0-all-deps.jar"
+    curl -LJO "https://github.com/google/google-java-format/releases/download/v1.16.0/google-java-format-1.16.0-all-deps.jar"
     chmod 755 google-java-format-1.16.0-all-deps.jar
 fi
 cd ..
